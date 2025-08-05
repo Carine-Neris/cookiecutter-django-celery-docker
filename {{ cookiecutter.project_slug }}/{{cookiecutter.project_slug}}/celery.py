3014f9fb7c466cbef7a9b6022c3aa00ecb4a420e
Carine Neris <carine_neris@hotmail.com>
2025-08-05 16:35:48 -0300

    iniciando boilerplate

---
+++ {{ cookiecutter.project_slug }}/{{cookiecutter.project_slug}}/celery.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+from django.conf import settings
+from celery.schedules import crontab
+
+from celery import Celery
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.project_slug}}.settings')
+app = Celery('{{cookiecutter.project_slug}}')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+
+app.conf.timezone = 'America/Recife'
